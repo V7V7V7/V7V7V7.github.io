db0a4a050209834f952e95e2ddbe814a4e85ba9d
V7V7V7 <ubiitsa@list.ru>
2020-11-17 17:29:09 +0200

    foto

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ $(function() {
 
         slider.slick({
   slidesToShow: 1,
-  slidesToScroll: 1,
+  slidesToScroll:1,
   autoplay: true,
-  autoplaySpeed: 5000,
+  autoplaySpeed: 10000,
             arrows:false,
 
 });
